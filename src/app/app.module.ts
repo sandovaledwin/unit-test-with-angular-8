b4a354bd8fb881f367784c286cf9a17eb818a534
Edwin Sandoval <sandovaledwin@hotmail.com>
2020-02-03 00:20:35 -0600

    Added the Material Design UI Framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,43 @@
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { Exercise1Component } from "./exercise-1/exercise1.component";
 import { Exercise2Component } from "./exercise-2/exercise2.component";
+import { Exercise3Component } from "./exercise-3/exercise3.component";
 
 const routes: Routes = [
   { path: "exercise-1", component: Exercise1Component },
-  { path: "exercise-2", component: Exercise2Component }
+  { path: "exercise-2", component: Exercise2Component },
+  { path: "exercise-3", component: Exercise3Component }
 ];
 
 @NgModule({
-  declarations: [AppComponent, Exercise1Component, Exercise2Component],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [
+    AppComponent,
+    Exercise1Component,
+    Exercise2Component,
+    Exercise3Component
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/exercise-3/exercise3.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "exercise-3",
+  templateUrl: "./exercise3.component.html",
+  styleUrls: ["./exercise3.component.css"]
+})
+export class Exercise3Component {}
